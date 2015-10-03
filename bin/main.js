b0d8d25737bd1353a4dbb8e0898e69874d56714c
Matt Bell <mappum@gmail.com>
2015-10-02 21:40:55 -0700

    Better main output

--- bin/main.js
+++ bin/main.js
@@ -2,13 +2,22 @@
 
 var args = require('minimist')(process.argv)
 var path = require('path')
+var W = require('watt').run
+require('colors')
 
 var INPM_PATH = process.env.INPM_PATH || path.join(process.env.HOME, '.inpm')
 process.env.INPM_PATH = INPM_PATH
 
 var inpm = require('../lib/inpm.js')(INPM_PATH)
 
-inpm.install(path.resolve('.'), function (err, res) {
-  if (err) return console.error(err.stack)
-  console.log('Installed successfully')
+W(function * (w) {
+  try {
+    var pkg = yield inpm.install(path.resolve('.'), w)
+    console.log('installed %s successfully'.green, (pkg.name + '@' + pkg.version).bold)
+    process.exit(0)
+  } catch (err) {
+    console.error('%s %s', 'error:'.red.bold, err.message)
+    console.error(err.stack.gray)
+    process.exit(1)
+  }
 })
